99083efda5476fb1000450f195554c833e7cac59
Josemaria Bolanos <josemabol@gmail.com>
2026-04-29 01:40:29 -0400

    Add button to Dixeo teacher toolbar

---
+++ lib.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Callbacks for block_dixeo_modulegen.
+ *
+ * @package    block_dixeo_modulegen
+ * @copyright  2026 Edunao SAS (contact@edunao.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Dixeo teacher toolbar: open the module generator sidebar (same capability as the block).
+ *
+ * @param \moodle_page $page
+ * @return array<int, array<string, mixed>>
+ */
+function block_dixeo_modulegen_add_button_to_teacher_toolbar(\moodle_page $page): array {
+    $path = $page->url->get_path();
+    $allowed = false;
+    foreach (['/course/view.php', '/course/section.php'] as $fragment) {
+        if (str_contains($path, $fragment)) {
+            $allowed = true;
+            break;
+        }
+    }
+    if (!$allowed || empty($page->course->id)) {
+        return [];
+    }
+
+    $context = \context_course::instance($page->course->id);
+    if (!has_capability('moodle/course:manageactivities', $context)) {
+        return [];
+    }
+
+    if ($page->requires->should_create_one_time_item_now('block_dixeo_modulegen')) {
+        $page->requires->css('/blocks/dixeo_modulegen/styles.css');
+        $page->requires->js_call_amd('block_dixeo_modulegen/activitychooser', 'init', [$page->course->id]);
+    }
+
+    $label = get_string('aiactivities', 'block_dixeo_modulegen');
+
+    return [[
+        'key' => 'generator',
+        'icon' => 'generator',
+        'label' => $label,
+        'title' => $label,
+        'dataaction' => 'open-dixeo-generator',
+        'controls' => 'dixeo-generator-panel',
+        'ismobileprimary' => true,
+        'isaccent' => true,
+        'islink' => false,
+    ]];
+}
